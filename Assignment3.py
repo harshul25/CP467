26596fc7c611916162b051e8bbbd467747990ba5
harshul25 <harshul.mehta863@gmail.com>
2022-10-06 19:04:42 -0400

    done A3

--- Assignment3.py
+++ Assignment3.py
@@ -14,43 +14,39 @@ for img in os.listdir(directory):
     if os.path.isfile(path):
         #open the image
         image = cv2.imread(path)
-        #save all the x values --> aspect ratio width/height
-        x_value.append(float(image.shape[1]/image.shape[0]))
-
         #for half the image analysis we split the image in two on the basis of width.
-        centre = int(0.5*len(image))
         [original_y,original_x,_] = image.shape
-        image_left = image[0:centre]
-        image_right = image[centre:]
-#left image
-        [y,x,_] = image_left.shape
+        max_x,max_y,min_x,min_y = 0,0,original_x,original_y
+        for i in range(original_x):
+            for j in range(original_y):
+                colour = image[j][i]
+                if colour[0] <= 52 and colour[1] <= 52 and colour[2]<=54:
+                    max_x = max(max_x,i)
+                    max_y = max(max_y,j)
+                    min_x = min(min_x,i)
+                    min_y = min(min_y,j)
+        #save all the x values --> aspect ratio width/height
+        x_value.append(float(abs(max_x - min_x)/abs(max_y-min_y)))
+        centre = int(0.5*(max_x-min_x))
+        centre += min_x
+        #left image
         counter = 0
-        for i in range(x):
-            for j in range(y):
-                colour = image_left[j][i]
+        for i in range(min_x,centre):
+            for j in range(min_y,max_y):
+                colour = image[j][i]
                 #pick on shades of black
                 if colour[0] <= 52 and colour[1] <= 52 and colour[2]<=54:
-                    if counter == 0:
-                        x_first_bounding_box = i
-                        y_first_bounding_box = j 
-                    counter +=1
-                    x_last_bounding_box = i 
-                    y_last_bounding_box = j 
-        if (y_first_bounding_box == y_last_bounding_box):
-            y_last_bounding_box += 1
-        if (x_first_bounding_box == x_last_bounding_box):
-            x_last_bounding_box += 1
-        y_value.append(float(counter/(abs(x_last_bounding_box - x_first_bounding_box) * abs(y_last_bounding_box - y_first_bounding_box))))
-#right image
-        [y,x,_] = image_right.shape
+                    counter +=1        
+        y_value.append(float(counter/(abs(max_x - min_x) * abs(max_y - min_y))))
+        #right image
         counter = 0
-        for i in range(x):
-            for j in range(y):
-                colour = image_right[j][i]
+        for i in range(centre,max_x):
+            for j in range(min_y,max_y):
+                colour = image[j][i]
                 #pick on shades of black
                 if colour[0] <= 52 and colour[1] <= 52 and colour[2]<=54:
                     counter +=1
-        z_value.append(float(counter/(abs(x_last_bounding_box - x_first_bounding_box) * abs(y_last_bounding_box - y_first_bounding_box))))
+        z_value.append(float(counter/(abs(max_x - min_x) * abs(max_y - min_y))))
 
 
 print(" x  \t y  \t z")
@@ -58,14 +54,17 @@ for i in range(len(x_value)-1):
     print("{:0.3f}  {:0.3f}  {:0.3f}".format(x_value[i], y_value[i], z_value[i]))
 
 
-#Result:
-# x       y       z
-#0.744  0.071  0.130
-#0.728  0.066  0.090
-#0.695  0.059  0.114
-#0.701  0.039  0.067
-#0.737  0.061  0.052
-#0.707  0.035  0.061
-#0.712  0.088  0.084
-#0.723  0.119  0.053
-#0.718  0.069  0.107        
+"""
+Result: 
+
+ x       y       z
+0.588  0.202  0.337
+0.630  0.226  0.300
+0.669  0.325  0.165
+0.433  0.322  0.312
+0.635  0.390  0.208
+0.464  0.265  0.326
+0.854  0.238  0.194
+0.565  0.208  0.362
+0.453  0.361  0.304
+"""       
